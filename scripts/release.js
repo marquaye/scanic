f9d8014c654c821ffb668e5fd1598a840ae397c0
marquaye <33137363+marquaye@users.noreply.github.com>
2025-07-09 21:34:46 +0200

    refactor: switch to ES module syntax for imports in release script

--- scripts/release.js
+++ scripts/release.js
@@ -5,9 +5,9 @@
  * Usage: npm run release [patch|minor|major]
  */
 
-const { execSync } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 const releaseType = process.argv[2] || 'patch';
 
